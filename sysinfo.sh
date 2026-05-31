1f8e2a5815ad05ab3b7ff888ed12efc78e4731ff
Alan <amiguelperez@proton.me>
2026-05-30 23:23:02 -0500

    first commit

---
+++ sysinfo.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+echo "=== INFO DEL SISTEMA ==="
+echo ""
+echo "--- CPU ---"
+cat /proc/cpuinfo | grep "model name" | head -1
+echo ""
+echo "--- RAM ---"
+cat /proc/meminfo | head -2
+echo ""
+echo "--- SISTEMA ---"
+uname -a
+
