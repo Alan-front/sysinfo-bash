f257e0aba14b7f003150b8abc91a52017be0b73e
Alan <amiguelperez@proton.me>
2026-06-11 22:34:13 -0500

    improve system info script

--- sysinfo.sh
+++ sysinfo.sh
@@ -1,37 +1,36 @@
-
 #!/bin/bash
 echo "=== INFO DEL SISTEMA ==="
+
 echo ""
 echo "--- CPU ---"
 cat /proc/cpuinfo | grep "model name" | head -1
+
 echo ""
 echo "--- RAM ---"
 cat /proc/meminfo | head -2
 
-echo "" 
-echo "--- DISCO ---" 
-df -H
+echo ""
+echo "--- DISCO ---"
+df -h
 
 echo ""
 echo "--- USUARIO ---"
 whoami
 
-
 echo ""
 echo "--- PROCESOS ACTIVOS ---"
-ps aux | wc -l
+ps aux | head -5
 
 echo ""
-echo  "--- FECHA Y HORA ---"
+echo "--- FECHA Y HORA ---"
 date
 
 echo ""
-echo "---VARIABLES DE ENTORNO ---"
+echo "--- VARIABLES DE ENTORNO ---"
 echo "HOME: $HOME"
-echo "PATH: $PATH"
 
 echo ""
 echo "--- SISTEMA ---"
-
 uname -a
+
 
